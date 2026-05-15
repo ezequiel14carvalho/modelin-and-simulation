c82f8e4bb622b0276dca42275617cc39e762dc23
Ezequiel Gomes de Carvalho <ezequielcarvalhop14@gmail.com>
2026-05-15 15:58:26 -0300

    ep7 V1

--- Exercício-Programa 7/ep7.py
+++ Exercício-Programa 7/ep7.py
@@ -1,12 +1,29 @@
-# 1. Importando a biblioteca com o apelido padrão 'np'
 import numpy as np
+import matplotlib.pyplot as plt
 
-# 2. Criando uma lista de números (array) para testar
-numeros = np.array([10, 20, 30, 40, 50])
+N = np.array([100, 1000, 10000])
+estimates_pi = []
 
-# 3. Usando a função de média que vimos antes
-resultado_media = np.mean(numeros)
+for n in N:
+    x = np.random.uniform(0, 1, n)
+    y = np.random.uniform(0, 1, n)
 
-# 4. Exibindo o resultado na tela
-print("O NumPy foi importado com sucesso!")
-print("A média dos números é:", resultado_media)
+    inside = x**2 + y**2 <= 1
+    pi_estimate = 4 * np.mean(inside)
+    estimates_pi.append(pi_estimate)
+
+print("Estimativas de Pi:", estimates_pi)
+
+# CORREÇÃO 1: A ordem correta é fig, ax
+plt.figure(figsize=(10, 6))
+
+plt.scatter(x[inside])
+
+# CORREÇÃO 2: Ajustando os nomes dos eixos de acordo com o plot
+ax.set_xlabel('N (Número de Pontos)')
+ax.set_ylabel('Estimativa de Pi')
+ax.set_title('Convergência de Monte Carlo para o valor de Pi')
+ax.grid(True)
+
+# CORREÇÃO 3: O show vem do plt
+plt.show()
